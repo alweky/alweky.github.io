ad0f78abcdd7dcdf575e103b572de3abede79389
alweky <alweky@yahoo.ca>
2015-02-27 21:50:43 -0500

    Some minor changes

--- mobile/scripts/mobile.js
+++ mobile/scripts/mobile.js
@@ -1,23 +1,9 @@
-var ie = 0, ff = 0, ch = 0, sa = 0;
+/*	mobile.js
+	Kevin Sau Yeung Kam
+	My Website
+	This is an js file for my mobile website
+*/
 
-/* $('.slider').glide({
-	autoplay: 5000,
-	arrows: 'body',
-	navigation: 'body'
-}); */
-
-/* $(document).ready(function () {
-    //initialize swiper when document ready  
-    var mySwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-	  pagination: '.swiper-pagination',
-      paginationClickable: '.swiper-pagination',
-	  autoplay: 2500
-    })        
-}); 
- */
  $(document).ready(function(){
 	$("#my_slider").muslider({
 		"responsive": true,
@@ -44,33 +30,5 @@ var ie = 0, ff = 0, ch = 0, sa = 0;
 	});
 });
  
-$(function () {
- /*    $("#sliderHome").excoloSlider({
-        mouseNav: false,
-        interval: 2000,
-        playReverse: true,
-		autoSize: true,
-		height: 250
-    });
-	$("#sliderHealthApp").excoloSlider({
-        mouseNav: false,
-        interval: 2000,
-        playReverse: true,
-		autoSize: true,
-		height: 650
-    });
-	$("#sliderAdvancedSpaceWarsApp").excoloSlider({
-        mouseNav: false,
-        interval: 2000,
-        playReverse: true,
-		autoSize: true,
-		height: 650
-    }); */
-	
-});
-
-
- 
 function init(){	
-	//$('.slider').glide();
 }
